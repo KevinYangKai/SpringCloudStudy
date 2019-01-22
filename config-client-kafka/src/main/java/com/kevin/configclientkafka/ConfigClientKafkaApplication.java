909bb31f1109e84ff32892282fedc4615bc744a0
KevinYangKai <15000737431@163.com>
2019-01-22 23:22:40 +0800

    总线和stream

---
+++ config-client-kafka/src/main/java/com/kevin/configclientkafka/ConfigClientKafkaApplication.java
@@ -0,0 +1,15 @@
+package com.kevin.configclientkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClientKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientKafkaApplication.class, args);
+    }
+
+}
